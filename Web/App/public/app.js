fde9099e0117ec105261d0b0af32d9723f02219c
Toka <toka.eldorghamy1@gmail.com>
2020-02-22 07:26:21 +0200

    Removed Feature of Starting plot with Start Button. Added New trace for Current.

--- Web/App/public/app.js
+++ Web/App/public/app.js
@@ -1,11 +1,17 @@
 var Speed = 0;
+var Current = 0;
+
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     console.log(app);
-
+    var database = firebase.database();
+    firebase.database().ref('users/' + '').set({
+      username: "Toqa",
+      email: "toka.eldoghamy1@gmail.com",
+      profile_picture : ""
+    });
+    //var dbRef = firebase.database().ref('Data');
     
-    var dbRef = firebase.database().ref().child('Measure/Speed');
-    dbRef.on('value', snap => Speed = snap.val());
 }
 );
 
