9297b0a6109aeada62e309b1071748b2d1236810
Toka <toka.eldorghamy1@gmail.com>
2020-02-22 19:07:34 +0200

    .

--- Web/App/public/app.js
+++ Web/App/public/app.js
@@ -3,12 +3,6 @@ document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     console.log(app);
     var database = firebase.database();
-    firebase.database().ref('users/' + '').set({
-      username: "Toqa",
-      email: "toka.eldoghamy1@gmail.com",
-      profile_picture : ""
-    });
-    
 }
 );
 
