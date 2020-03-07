28647a2a617e12bf8fb31e9d77e1261d224f9fee
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2020-03-07 21:45:33 +0200

    add python file for the server side.
    Use flask for the server. It will handle json files.

---
+++ Web/App/Flask/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import flask_cors
+import json
+
+app = Flask(__name__, static_folder='assets')
+
+# read json file
+F = open("file.json","r")
+# turn the data into dictionary
+data_store = json.load(F)
+
+
+@app.route("/readjsonfile", methods=['GET'])
+def read_json_file():
+    js_object = make_response(jsonify(data_store), 200)
+    return js_object
+
+
+# making json file
+@app.route("/writejsonfile", methods=['POST'])
+def write_json_file():
+    write_file = open("write_file.json", "w")
+    if request.json:
+        req = request.get_json()
+        json.dump(req, write_file)
+
+    return "Saved"
+
+
+@app.route("/hello", methods=['GET'])
+def hello():
+    return "hello"
+
+@app.route("/json", methods=["POST"])
+def json_example():
+
+    if request.json:
+        req = request.get_json()
+        response_body = {
+            "message": "JSON received!",
+            "sender": req.get("Name")
+        }
+        res = make_response(jsonify(response_body), 200)
+        return res
+    else:
+        return make_response(jsonify({"message": "Request body must be JSON"}), 400)
+
+@app.route("/", methods=['GET'])
+def student():
+    return render_template("index.html")
+
+
+# for CORS
+flask_cors.CORS(app)
+
+# debug is for restarting whenever app.py changes
+app.run(debug=True)
+
